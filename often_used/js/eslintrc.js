1dc295a80d022797f0d8c6e725adb49e3b4731a8
virus <virusbb001a@gmail.com>
2016-05-29 23:54:34 +0900

    add eslintrc

---
+++ often_used/js/eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true,
+    "mocha"; true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2,
+      {
+        "VariableDeclarator": {
+          "var": 2,
+          "let": 2,
+          "const": 3
+        }
+      }
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
