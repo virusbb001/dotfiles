6b66d016eea2d578541570895b0693d7985fa196
virus <virusbb001a@gmail.com>
2017-01-10 08:17:02 +0900

    fix installer's syntax error

--- installer.sh
+++ installer.sh
@@ -15,7 +15,7 @@ else
  echo ".zshrc already exists. skip"
 fi
 
-if [[! -f ~/.tmux.conf ]]; then
+if [[ ! -f ~/.tmux.conf ]]; then
  install -m 0644 dot_tmux_conf ~/.tmux.conf
 else
  echo ".tmux.conf already exists. skip"
