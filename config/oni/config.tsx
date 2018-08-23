3d5bd5040d9cc903df03d54a2e1248bb39b19438
virus <virusbb001a@gmail.com>
2018-08-24 01:42:25 +0900

    oni: change settings

--- config/oni/config.tsx
+++ config/oni/config.tsx
@@ -4,6 +4,8 @@ import * as Oni from "oni-api";
 export const activate = (oni: Oni.Plugin.Api) => {
   oni.input.unbind("<c-g>") // make C-g work as expected in vim
   oni.input.unbind("<c-v>") // make C-v work as expected in vim
+  oni.input.unbind("<f3>")
+  oni.input.unbind("<c-p>");
   oni.input.bind("<s-c-g>", () => oni.commands.executeCommand("sneak.show")) // You can rebind Oni's behaviour to a new keybinding
 }
 
@@ -16,14 +18,15 @@ export const configuration = {
   "commandline.mode"         : false, // Do not override commandline UI
   "wildmenu.mode"            : false, // Do not override wildmenu UI,
   "statusbar.enabled"        : false, // use vim's default statusline
-  "sidebar.enabled"          : false, // sidebar ui is gone
+  "sidebar.enabled"          : true,
   "sidebar.default.open"     : false, // the side bar collapse 
-  "learning.enabled"         : false, // Turn off learning pane
-  "achievements.enabled"     : false, // Turn off achievements tracking / UX
+  "learning.enabled"         : true,
+  "achievements.enabled"     : true,
   "editor.typingPrediction"  : false, // Wait for vim's confirmed typed characters, avoid edge cases
   "editor.textMateHighlighting.enabled" : false, // Use vim syntax highlighting
   "editor.fontSize": "14px",
   "editor.fontFamily": "Consolas,Inconsolata,Noto Sans Mono CJK JP, Monaco",
   "editor.scrollBar.visible": false,
   "editor.scrollBar.cursorTick.visible": false,
+  "editor.completions.mode": "native",
 }
