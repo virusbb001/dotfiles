ee92759d809a08198234cb38331fa9df56163723
virus <virusbb001a@gmail.com>
2019-02-25 00:35:37 +0900

    move ~/.* file to home/

--- installer.sh
+++ installer.sh
@@ -3,25 +3,25 @@
 cd "$(dirname "$0")" || exit 1
 
 if [ ! -f ~/.vimrc ]; then
- install -m 0644 dot_vimrc ~/.vimrc
+ install -m 0644 home/dot_vimrc ~/.vimrc
 else
  echo ".vimrc already exists. skip"
 fi
 
 if [ ! -f ~/.zshrc ]; then
- install -m 0644 dot_zshrc ~/.zshrc
+ install -m 0644 home/dot_zshrc ~/.zshrc
 else
  echo ".zshrc already exists. skip"
 fi
 
 if [ ! -f ~/.zshenv ]; then
- install -m 0644 dot_zshenv ~/.zshenv
+ install -m 0644 home/dot_zshenv ~/.zshenv
 else
  echo ".zshenv already exists. skip"
 fi
 
 if [ ! -f ~/.tmux.conf ]; then
- install -m 0644 dot_tmux_conf ~/.tmux.conf
+ install -m 0644 home/dot_tmux_conf ~/.tmux.conf
 else
  echo ".tmux.conf already exists. skip"
 fi
