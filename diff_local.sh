d1370cb8c3107f9e778801be57d8f8156096c3a4
virus <virusbb001a@gmail.com>
2019-02-26 22:38:17 +0900

    change copy file

--- diff_local.sh
+++ diff_local.sh
@@ -6,10 +6,10 @@ cp $HOME/.vim/local_settings.vim .
 cp $HOME/.vim/dein.toml dein_local.toml
 cp $HOME/.vim/dein_lazy.toml dein_lazy_local.toml
 
-cp $HOME/.xonshrc xonshrc
+cp $HOME/.xonshrc home/dot_xonshrc
 cp $HOME/.config/oni/config.tsx oni_config.tsx
-cp $HOME/.vimrc dot_vimrc
-cp $HOME/.gvimrc dot_gvimrc
+cp $HOME/.vimrc home/dot_vimrc
+cp $HOME/.gvimrc home/dot_gvimrc
 
 git -C $dotfiles add -N $dotfiles
 git -C $dotfiles diff > diff_dotfiles.patch
