420fa49dd7d2e9dbf14c09571db2a46f27ec3cac
virus <virusbb001a@gmail.com>
2019-03-27 18:51:02 +0900

    ps: remove sudo and change output of fix_tmpdir

--- profile.ps1
+++ profile.ps1
@@ -1,36 +1,14 @@
 ﻿Set-StrictMode -Version Latest
 
-function sudo {
- <#
- $file, [string]$arguments = $args;
-
- if (!$file) {
-  Write-Host "sudo [command] [args]"
-  return
- }
- 
- $psi = New-Object System.Diagnostics.ProcessStartInfo $file;
- $psi.Arguments = $arguments;
- $psi.Verb = "runas";
- $psi.WorkingDirectory = Get-Location;
- [System.Diagnostics.Process]::Start($psi);
- #>
- if ($args.Length -lt 1){
-  Write-Host "sudo command"
-  return
- }
- 
- powershell -command "Start-Process -Verb runas $args"
-}
-
 function list_neovim {
   [System.IO.Directory]::GetFiles("\\.\\pipe\\") | ? { $_.Contains("nvim") }
 }
 
 function fix_tmpdir {
    list_neovim | % {
+     Write-Host "processing $_"
      $dir = Split-Path (nvr --servername $_ --remote-expr 'tempname()') -Parent
-     write-Host "progressing ${dir}"
+     write-Host "`tprogressing ${dir}"
      if (!(Test-Path -Path $dir)) {
        New-Item -ItemType Directory -Path $dir
        Write-Host "Created ${dir}"
