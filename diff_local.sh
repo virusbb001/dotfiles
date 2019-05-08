b677b6e7088d74098baa5927658e8d4f59b2e9a6
virus <virusbb001a@gmail.com>
2019-05-09 01:08:16 +0900

    update diff_local.sh

--- diff_local.sh
+++ diff_local.sh
@@ -2,14 +2,20 @@
 
 dotfiles=$HOME/dotfiles
 
-cp $HOME/.vim/local_settings.vim .
-cp $HOME/.vim/dein.toml dein_local.toml
+if [ -f $HOME/.vim/local_settings.vim ]; then
+  cp $HOME/.vim/local_settings.vim .
+fi
+if [ -f $HOME/.vim/dein.toml ]; then
+  cp $HOME/.vim/dein.toml dein_local.toml
+fi
+if [ -f $HOME/.vim/dein_lazy.toml ]; then
 cp $HOME/.vim/dein_lazy.toml dein_lazy_local.toml
+fi
 
-cp $HOME/.xonshrc home/dot_xonshrc
+cp $HOME/.xonshrc home/.xonshrc
 cp $HOME/.config/oni/config.tsx oni_config.tsx
-cp $HOME/.vimrc home/dot_vimrc
-cp $HOME/.gvimrc home/dot_gvimrc
+cp $HOME/.vimrc home/.vimrc
+cp $HOME/.gvimrc home/.gvimrc
 
 git -C $dotfiles add -N $dotfiles
 git -C $dotfiles diff > diff_dotfiles.patch
