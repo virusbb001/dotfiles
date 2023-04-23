0fbd5f92ce8148aa49efca8005ac4f049418979f
virus <2017646+virusbb001@users.noreply.github.com>
2023-04-24 01:16:17 +0900

    vim: add format-ts-errors.nvim

--- vim/dein/lsp_settings.lua
+++ vim/dein/lsp_settings.lua
@@ -105,7 +105,43 @@ function _G.virus_lsp_settings ()
     root_dir = detect_deno_root_dir
   }))
   nvim_lsp.tsserver.setup(base_lsp_with({
-    root_dir = detect_node_root_dir
+    root_dir = detect_node_root_dir,
+    handlers = {
+      ["textDocument/publishDiagnostics"] = function(
+        _,
+        result,
+        ctx,
+        config
+      )
+        if result.diagnostics == nil then
+          return
+        end
+
+        -- ignore some tsserver diagnostics
+        local idx = 1
+        while idx <= #result.diagnostics do
+          local entry = result.diagnostics[idx]
+
+          local formatter = require('format-ts-errors')[entry.code]
+          entry.message = formatter and formatter(entry.message) or entry.message
+
+          -- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
+          if entry.code == 80001 then
+            -- { message = "File is a CommonJS module; it may be converted to an ES module.", }
+            table.remove(result.diagnostics, idx)
+          else
+            idx = idx + 1
+          end
+        end
+
+        vim.lsp.diagnostic.on_publish_diagnostics(
+          _,
+          result,
+          ctx,
+          config
+        )
+      end,
+    }
   }))
 
   local function virus_lsp_after_denops ()
