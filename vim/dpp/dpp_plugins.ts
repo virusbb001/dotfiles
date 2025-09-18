77cb29b5d8bf0e793e2cd5d6f649c791e074b867
virus <2017646+virusbb001@users.noreply.github.com>
2025-09-18 14:51:53 +0900

    add DppUpdate

--- vim/dpp/dpp_plugins.ts
+++ vim/dpp/dpp_plugins.ts
@@ -1,3 +1,4 @@
+import * as path from "jsr:@std/path";
 import { basename } from "jsr:@std/path/basename";
 import { type Plugin } from "jsr:@shougo/dpp-vim@~3.1.0/types"
 
@@ -8,10 +9,18 @@ export default async function getDppPlugins(): Promise<Plugin[]> {
     }
   })).default;
 
+  const extraSettings: Record<string, Partial<Plugin>> = {
+    "dpp.vim": {
+      hooks_file: path.join(import.meta.dirname!, "hooks/dpp.vim")
+    }
+  };
+
   const plugins = plugin_map.minimal.concat(plugin_map.install).map(plugin_repo => {
+    const name = basename(plugin_repo);
     return {
       repo: plugin_repo,
-      name: basename(plugin_repo)
+      name,
+      ...(extraSettings[name] ?? {})
     };
   });
 
---
+++ vim/dpp/hooks/dpp.vim
@@ -0,0 +1,3 @@
+" hook_source {{{
+command DppUpdate call dpp#async_ext_action('installer', 'update')
+" }}}
