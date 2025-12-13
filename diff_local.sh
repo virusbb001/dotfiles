de58b8fd297f35bc23444c80e156844edb800d71
virus <2017646+virusbb001@users.noreply.github.com>
2025-12-14 03:10:51 +0900

    remove check dein files

--- diff_local.sh
+++ diff_local.sh
@@ -5,12 +5,6 @@ dotfiles=$HOME/dotfiles
 if [ -f $HOME/.vim/local_settings.vim ]; then
   cp $HOME/.vim/local_settings.vim .
 fi
-if [ -f $HOME/.vim/dein.toml ]; then
-  cp $HOME/.vim/dein.toml dein_local.toml
-fi
-if [ -f $HOME/.vim/dein_lazy.toml ]; then
-cp $HOME/.vim/dein_lazy.toml dein_lazy_local.toml
-fi
 
 cp $HOME/.xonshrc home/.xonshrc
 cp $HOME/.vimrc home/.vimrc
